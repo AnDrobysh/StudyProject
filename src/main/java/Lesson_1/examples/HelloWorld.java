867c3beec8f813154550136718c9353bf23a8564
Дробыш Андрей <adrobysh@alfabank.ru>
2024-08-26 21:23:02 +0300

    add presentation

--- src/main/java/Lesson_1/examples/HelloWorld.java
+++ src/main/java/Lesson_1/examples/HelloWorld.java
@@ -2,6 +2,9 @@ package Lesson_1.examples;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        String x = "Hello world";
+        int y = 12;
+        System.out.println(x);
+
     }
 }
--- src/main/java/Lesson_1/examples/SwitchCase.java
+++ src/main/java/Lesson_1/examples/SwitchCase.java
@@ -2,7 +2,7 @@ package Lesson_1.examples;
 
 public class SwitchCase {
     public static void main(String[] args){
-        int day = 3;
+        int day = 12;
         switch (day) {
             case 1:
                 System.out.println("Monday");
@@ -13,8 +13,9 @@ public class SwitchCase {
             case 3:
                 System.out.println("Wednesday");
                 break;
-            default:
-                System.out.println("Another day");
+            case 4:
+                System.out.println("pidaras");
+                break;
         }
     }
 }
