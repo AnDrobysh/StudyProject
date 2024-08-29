08a1d70a3908109d926e23a3a1dff967f634a8e1
AnDrobysh <66019768+androbysh@users.noreply.github.com>
2024-08-30 01:38:07 +0300

    Merge pull request #2 from AnDrobysh/AndreyDrobysh
    add lesson2

--- src/main/java/Lesson_1/examples/TernaryOperator.java
+++
@@ -1,9 +0,0 @@
-package Lesson_1.examples;
-
-public class TernaryOperator {
-    public static void main(String[] args) {
-        int age = 18;
-        String message = age >= 18 ? "Adult" : "Minor";
-        System.out.println(message);
-    }
-}
