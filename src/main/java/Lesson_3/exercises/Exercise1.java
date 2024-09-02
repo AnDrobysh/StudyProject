39b3ea1131af9bfdba6ad0e374100d9377b3ccf5
Andrey Drobysh <adrobysh@alfabank.ru>
2024-09-02 20:13:48 +0300

    add lesson3

---
+++ src/main/java/Lesson_3/exercises/Exercise1.java
@@ -0,0 +1,16 @@
+package Lesson_3.exercises;
+/*
+Код должен суммировать все элементы массива numbers и вывести результат, но он содержит ошибки. Найдите и исправьте их.*/
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        int[] numbers = {4, 7, 1, 9, 3};
+        int sum = 0;
+
+        for (int i = 0; i <= numbers.length; i++) { // Ошибка в условии цикла
+            sum += numbers[i]; // Может произойти выход за пределы массива
+        }
+
+        System.out.println("Сумма элементов массива: " + sum);
+    }
+}
